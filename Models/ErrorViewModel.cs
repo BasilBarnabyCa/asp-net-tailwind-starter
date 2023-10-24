02f3162ba1676742137841817b5a4ffc33dc639a
Basil Barnaby <basil.barnaby@devmassive.com>
2023-10-24 16:56:49 -0400

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace TailwindStarter.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
